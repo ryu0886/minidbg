1deda208fa7dba9e00eecf242979dcaefecd6b8a
ryan <38020822+ryu0886@users.noreply.github.com>
2024-03-21 21:10:13 +0800

    Update version.h

--- main/version.h
+++ main/version.h
@@ -3,9 +3,9 @@
 
 #define V_MAJOR     0
 #define V_MINOR     9
-#define V_REV       2
-#define V_BUILD     13192
-#define V_VERSION   "0.9.2.13192"
+#define V_REV       3
+#define V_BUILD     13211
+#define V_VERSION   "0.9.3.13211"
 #define V_PRODUCT   "Cloud Scanner"
 #define V_APPNAME   "winapp"
 #define V_COMPANY   "WTH"
